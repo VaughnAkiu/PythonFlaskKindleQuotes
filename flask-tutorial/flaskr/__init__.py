00046f555cbfcbfbb2ca0cbe4f2f4e3598b9bef1
Vaughn Akiu <vkakiu@gmail.com>
2021-01-12 10:49:44 -0500

    create a blueprint, import and register it from the factory

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -40,5 +40,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    #import and register the blueprint from factory
+    from . import auth
+    app.register_blueprint(auth.bp)
         
     return app    
