713b83d28827cb79ff9fced80f9c555d96a7366d
Vaughn Akiu <vkakiu@gmail.com>
2021-01-12 13:45:40 -0500

    create a decorator to handle login_required events

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -83,3 +83,12 @@ def register():
         return redirect(url_for('index'))
 
     
+    def login_required(view):
+        @functools.wrap(view)
+        def wrapped_view(**kwargs):
+            if g.user is None:
+                return redirect(url_for('auth.login'))
+
+            return view(**kwargs)
+
+        return wrapped_view
