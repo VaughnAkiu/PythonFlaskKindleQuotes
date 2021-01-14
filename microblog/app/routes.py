826f5013e0d75698fa0ce8b0999b9c83026876da
Vaughn Akiu <vkakiu@gmail.com>
2021-01-14 13:40:12 -0500

    creating decorators for browser to call simple hello world return

---
+++ microblog/app/routes.py
@@ -0,0 +1,10 @@
+#routes are different URLs which the application implements
+#handlers for these routes written as Python functions are called view functions
+
+from app import app
+
+#these two decorators translates to when a WEb browser requests, / or /index, the function will pass index(), which returns hello,... etc
+@app.route('/')         #decorator (modifies the function that follows)
+@app.route('/index')    #decorator
+def index():
+    return "hello, hellooooooooo"
