7631b6d531515f13ee3c3ed637d61f1a3740f4ad
Vaughn Akiu <vkakiu@gmail.com>
2021-02-03 15:04:58 -0500

    add view function to create a form for rendering

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,8 @@
 #routes are different URLs which the application implements
 #handlers for these routes written as Python functions are called view functions
-
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 #these two decorators translates to when a WEb browser requests, / or /index, the function will pass index(), which returns hello,... etc
 @app.route('/')         #decorator (modifies the function that follows)
@@ -21,6 +22,10 @@ def index():
 
     return render_template('index.html', title='Home', user=user, posts=posts) #takes template filename, variable list of template arguments, returns that template with placeholder now with actual values
 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
 
 
