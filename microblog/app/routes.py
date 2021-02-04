e35aa0c0448f051ba9407a74f56c6eaadc9e7015
Vaughn Akiu <vkakiu@gmail.com>
2021-02-04 14:48:12 -0500

    adding get and post functionality

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,6 @@
 #routes are different URLs which the application implements
 #handlers for these routes written as Python functions are called view functions
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -22,9 +22,13 @@ def index():
 
     return render_template('index.html', title='Home', user=user, posts=posts) #takes template filename, variable list of template arguments, returns that template with placeholder now with actual values
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
 
 
